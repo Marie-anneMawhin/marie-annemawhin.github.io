de95a73352717dcec744bb70322db0cb3457249b
Paul Clark <clark.paul123@gmail.com>
2020-09-26 15:39:06 +0100

    feat: 🎸 add fixed width to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import NavBar from './components/NavBar'
 function App() {
   return (
     <div className="App vh-100 avenir">
-      <div className="fl w-25 h-100 pa3 bg-navy light-gray o-80">
+      <div className="fl w-200 h-100 pa3 bg-navy light-gray o-80">
         <NavBar />
       </div>
-      <div className="fl w-75 pa3">
+      <div className="fl pa3">
         <p className="gold">Hello</p>
       </div>
     </div>
