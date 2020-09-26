c6e1d9b9d4da22492f00419ca1fdee8310a441e8
Paul Clark <clark.paul123@gmail.com>
2020-09-26 15:29:07 +0100

    feat: 🎸 add list to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,17 @@ class NavBar extends Component {
   render() {
     return (
       <div>
-        <p>Hello I'm a navbar</p>
+        <h1 className="f4 bold center mw6">Marie-anne Mawhin</h1>
+        <ul className="list pl0 ml0 center mw6">
+          <li className="ph3 pv3 bb b--light-silver">Contact</li>
+          <li className="ph3 pv3 bb b--light-silver">Skills</li>
+          <li className="ph3 pv3 bb b--light-silver">Languages</li>
+          <li className="ph3 pv3 bb b--light-silver">Interests</li>
+          <li className="ph3 pv3 bb b--light-silver">Experience</li>
+          <li className="ph3 pv3 bb b--light-silver">Education</li>
+          <li className="ph3 pv3 bb b--light-silver">Projects</li>
+          <li className="ph3 pv3">Achievements</li>
+        </ul>
       </div>
     )
   }
