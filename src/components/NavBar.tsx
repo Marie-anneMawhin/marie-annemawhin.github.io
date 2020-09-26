5105164adeb0f2c899582a2f9895dbaa3303497c
Paul Clark <clark.paul123@gmail.com>
2020-09-26 16:27:44 +0100

    feat: 🎸 add font weight

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,42 +6,42 @@ class NavBar extends Component {
         <h1 className="f4 bold center mw6">Marie-anne Mawhin</h1>
         <ul className="list pl0 ml0 center mw6">
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Contact
             </a>
           </li>
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Skills
             </a>
           </li>
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Languages
             </a>
           </li>
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Interests
             </a>
           </li>
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Experience
             </a>
           </li>
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Education
             </a>
           </li>
           <li className="pv3 bb b--washed-blue">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Projects
             </a>
           </li>
           <li className="pv3">
-            <a className="f5 link dim washed-blue bg-navy" href="#0">
+            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
               Achievements
             </a>
           </li>
