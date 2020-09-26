bd057590cf6216ead2394ea8db80bb35e3bf4363
Paul Clark <clark.paul123@gmail.com>
2020-09-26 16:29:07 +0100

    fix: 🐛 sort width out again

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="fl w-third w-30-m w-20-l h-100 pa3 bg-navy washed-blue o-80">
         <NavBar />
       </div>
-      <div className="fl none w-70-m w-80-l pa3 h-100 bg-washed-blue">
+      <div className="fl w-two-thirds w-70-m w-80-l pa3 h-100 bg-washed-blue">
         <div className="">
           <p className="gold">Hello</p>
         </div>
