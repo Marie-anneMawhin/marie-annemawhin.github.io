76e6a7be7eb296231883b4b4820261c8b3dade60
Paul Clark <clark.paul123@gmail.com>
2020-09-26 16:12:20 +0100

    feat: 🎸 remove left padding in navbar items for better alignmen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,14 +5,14 @@ class NavBar extends Component {
       <div>
         <h1 className="f4 bold center mw6">Marie-anne Mawhin</h1>
         <ul className="list pl0 ml0 center mw6">
-          <li className="ph3 pv3 bb b--washed-blue">Contact</li>
-          <li className="ph3 pv3 bb b--washed-blue">Skills</li>
-          <li className="ph3 pv3 bb b--washed-blue">Languages</li>
-          <li className="ph3 pv3 bb b--washed-blue">Interests</li>
-          <li className="ph3 pv3 bb b--washed-blue">Experience</li>
-          <li className="ph3 pv3 bb b--washed-blue">Education</li>
-          <li className="ph3 pv3 bb b--washed-blue">Projects</li>
-          <li className="ph3 pv3">Achievements</li>
+          <li className="pv3 bb b--washed-blue">Contact</li>
+          <li className="pv3 bb b--washed-blue">Skills</li>
+          <li className="pv3 bb b--washed-blue">Languages</li>
+          <li className="pv3 bb b--washed-blue">Interests</li>
+          <li className="pv3 bb b--washed-blue">Experience</li>
+          <li className="pv3 bb b--washed-blue">Education</li>
+          <li className="pv3 bb b--washed-blue">Projects</li>
+          <li className="pv3">Achievements</li>
         </ul>
       </div>
     )
