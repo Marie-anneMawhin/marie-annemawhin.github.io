ec7f3959062a2ee30706310d66c6090a830a5c80
Paul Clark <clark.paul123@gmail.com>
2020-09-26 19:58:12 +0100

    refactor: 💡 convert navbar to FC and use map for repeated html

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,53 +1,29 @@
-import React, { Component, Fragment } from 'react'
-class NavBar extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1 className="f4 bold mw6">Marie-anne Mawhin</h1>
-        <ul className="list pl0 ml0 mw6">
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Contact
-            </a>
-          </li>
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Skills
-            </a>
-          </li>
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Languages
-            </a>
-          </li>
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Interests
-            </a>
-          </li>
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Experience
-            </a>
-          </li>
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Education
-            </a>
-          </li>
-          <li className="pv3 bb b--washed-blue">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Projects
-            </a>
-          </li>
-          <li className="pv3">
-            <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
-              Achievements
-            </a>
-          </li>
-        </ul>
-      </Fragment>
-    )
-  }
+import React, { Fragment } from 'react'
+
+const sectionsTitles = [
+  'Contact',
+  'Skills',
+  'Languages',
+  'Interests',
+  'Experience',
+  'Projects',
+  'Education',
+  'Achievements'
+]
+
+const NavBar: React.FC = () => {
+  const links = sectionsTitles.map((title) => (
+    <li className="pv3 bb b--washed-blue">
+      <a className="f5 fw5 link dim washed-blue bg-navy" href="#0">
+        {title}
+      </a>
+    </li>
+  ))
+  return (
+    <Fragment>
+      <h1 className="f4 bold mw6">Marie-anne Mawhin</h1>
+      <ul className="list pl0 ml0 mw6">{links}</ul>
+    </Fragment>
+  )
 }
 export default NavBar
