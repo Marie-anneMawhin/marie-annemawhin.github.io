1937c1cab6a029c56d3438876dfcdb6f46d74c1b
Paul Clark <clark.paul123@gmail.com>
2020-09-26 16:09:54 +0100

    feat: 🎸 add washed blue colour to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar'
 function App() {
   return (
     <div className="App vh-100 avenir">
-      <div className="fl w-third w-30-m w-20-l h-100 pa3 bg-navy light-gray o-80">
+      <div className="fl w-third w-30-m w-20-l h-100 pa3 bg-navy washed-blue o-80">
         <NavBar />
       </div>
       <div className="fl none w-70-m w-80-l pa3 h-100 bg-washed-blue">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,13 +5,13 @@ class NavBar extends Component {
       <div>
         <h1 className="f4 bold center mw6">Marie-anne Mawhin</h1>
         <ul className="list pl0 ml0 center mw6">
-          <li className="ph3 pv3 bb b--light-silver">Contact</li>
-          <li className="ph3 pv3 bb b--light-silver">Skills</li>
-          <li className="ph3 pv3 bb b--light-silver">Languages</li>
-          <li className="ph3 pv3 bb b--light-silver">Interests</li>
-          <li className="ph3 pv3 bb b--light-silver">Experience</li>
-          <li className="ph3 pv3 bb b--light-silver">Education</li>
-          <li className="ph3 pv3 bb b--light-silver">Projects</li>
+          <li className="ph3 pv3 bb b--washed-blue">Contact</li>
+          <li className="ph3 pv3 bb b--washed-blue">Skills</li>
+          <li className="ph3 pv3 bb b--washed-blue">Languages</li>
+          <li className="ph3 pv3 bb b--washed-blue">Interests</li>
+          <li className="ph3 pv3 bb b--washed-blue">Experience</li>
+          <li className="ph3 pv3 bb b--washed-blue">Education</li>
+          <li className="ph3 pv3 bb b--washed-blue">Projects</li>
           <li className="ph3 pv3">Achievements</li>
         </ul>
       </div>
