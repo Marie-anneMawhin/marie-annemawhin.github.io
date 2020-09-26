5b624158303ffdc563b4c85c1e9230f03d1cc459
Paul Clark <clark.paul123@gmail.com>
2020-09-26 14:43:16 +0100

    feat: 🎸 add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <p className="gold">Hello</p>
+    <div className="App mw9 center ph3-ns">
+      <div className="fl w-25 pa2">
+        <NavBar />
+      </div>
+      <div className="fl w-75 pa2">
+        <p className="gold">Hello</p>
+      </div>
     </div>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class Button extends Component {
+  render() {
+    return <p>Hello I'm a navbar</p>
+  }
+}
+export default Button
