38734f1448245cbd3b85dabdc896d2890e3508fe
Paul Clark <clark.paul123@gmail.com>
2020-09-27 10:16:11 +0100

    fix: 🐛 typescript, mobile padding

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Main from './components/Main'
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App vh-100 avenir">
-      <div className="fl w-third w-30-m w-20-l h-100 pa3 bg-navy washed-blue o-80">
+      <div className="fl w-third w-30-m w-20-l h-100 pa2 pa3-ns bg-navy washed-blue o-80">
         <NavBar />
       </div>
-      <div className="fl w-two-thirds w-70-m w-80-l pa3 h-100 overflow-y-scroll bg-washed-blue">
+      <div className="fl w-two-thirds w-70-m w-80-l pa2 pa3-ns h-100 overflow-y-scroll bg-washed-blue">
         <Main />
       </div>
     </div>
