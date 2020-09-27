d7b80a10e48c7a460850cd6a00a07b988f2129e0
Paul Clark <clark.paul123@gmail.com>
2020-09-27 11:24:24 +0100

    feat: 🎸 add skills section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react'
+import Skills from './Skills'
 import Education from './Education'
 import Publications from './Publications'
 class Main extends Component {
   render(): JSX.Element {
     return (
       <Fragment>
+        <Skills />
         <Education />
         <Publications />
       </Fragment>
---
+++ src/components/Skills.tsx
@@ -0,0 +1,105 @@
+import React, { Fragment } from 'react'
+import SectionHeader from './SectionHeader'
+
+const items = {
+  key: [
+    {
+      title: 'Programming',
+      skills: [
+        'Python (Panda, Seaborn, Matplotlib)',
+        'notions of R and SQL',
+        'Jython'
+      ]
+    },
+    {
+      title: 'Image Analysis',
+      skills: [
+        'notions of deep learning/ML in ImageJ',
+        'control GPU usage',
+        'Python in image processing'
+      ]
+    },
+    {
+      title: 'Medical Research',
+      skills: [
+        'bacteremia',
+        'cardiorenal syndrome',
+        'atherosclerosis',
+        'ischemic strokes',
+        'murine models',
+        'immunology',
+        'platelet biology'
+      ]
+    },
+    {
+      title: 'Techinical Biology',
+      skills: [
+        'genotyping',
+        'qPCR',
+        'Flow cytometry',
+        'Intravital',
+        'Microscopy',
+        'Histology',
+        'Molecular biology',
+        'Cell biology'
+      ]
+    },
+    {
+      title: 'Teamwork & soft skills',
+      skills: [
+        'more than 5 different teams',
+        'national/international collaborations',
+        'technical cooperation',
+        'supervision of interns'
+      ]
+    }
+  ],
+  other: [
+    {
+      title: 'Oral & written communication',
+      skills: ['international conferences', 'scientific writing', 'grants']
+    },
+    {
+      title: 'Other IT skills',
+      skills: [
+        'MS Office suite (Word, Excel, Ppt)',
+        'Photoshop',
+        'bibliography (PubMed, Mendeley)',
+        'Zen ASF lite',
+        'Diva',
+        'Flow Jo',
+        'ImageJ',
+        'Prism',
+        '...'
+      ]
+    },
+    {
+      title: 'Project Management',
+      skills: [
+        'time management',
+        'feasibility',
+        'contingency',
+        'management of two small teams (2 persons)'
+      ]
+    }
+  ]
+}
+
+const Skills: React.FC = () => {
+  const keySkillItems = items.key.map(({ title, skills }, index) => (
+    <div key={index} className="tc ph3">
+      <p className="f4 f3-m f2-l fw2 black-90 mv3">{title}</p>
+      <p className="f5 f4-m f3-l fw2 black-50 mt0 lh-copy">
+        {skills.join(', ')}
+      </p>
+    </div>
+  ))
+  return (
+    <Fragment>
+      <SectionHeader title="Skills" />
+      {keySkillItems}
+    </Fragment>
+  )
+}
+
+export default Skills
