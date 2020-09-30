8ef51171178d9b3229870fc5ca61b2e463fd38d6
Paul Clark <clark.paul123@gmail.com>
2020-09-30 21:11:29 +0100

    feat: 🎸 add icons, padding, colour to social links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,39 @@
 import React, { Fragment } from 'react'
 import SectionHeader from './SectionHeader'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconName } from '@fortawesome/fontawesome-common-types'
 
-const items = [
-  { title: 'LinkedIn', link: 'https://www.linkedin.com/in/marieanne-mawhin' },
-  { title: 'GitHub', link: 'https://github.com/Marie-anneMawhin' }
+const items: {
+  title: string
+  colour: string
+  link: string
+  iconName: IconName
+}[] = [
+  {
+    title: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/marieanne-mawhin',
+    colour: 'blue',
+    iconName: 'linkedin'
+  },
+  {
+    title: 'GitHub',
+    link: 'https://github.com/Marie-anneMawhin',
+    colour: 'dark-gray',
+    iconName: 'github'
+  }
 ]
 
 const Contact: React.FC = () => {
-  const contactItems = items.map(({ title, link }, index) => (
+  const contactItems = items.map(({ title, link, colour, iconName }, index) => (
     <p key={index} className="tc ph3 f4 f3-m f2-l fw2 mv3">
+      <span className={colour + ' pr2'}>
+        <FontAwesomeIcon icon={['fab', iconName]} />
+      </span>
       <a
         href={link}
         target="_blank"
         rel="noopener noreferrer"
-        className="black-90"
+        className="link dark-blue"
       >
         {title}
       </a>
