5e16c0a9f45807766ddf456f0f3099fb3c70b701
Paul Clark <clark.paul123@gmail.com>
2020-10-02 20:35:25 +0100

    Updates

--- precache-manifest.4a3af9760d36141b847db366ec84e0dd.js
+++ precache-manifest.4a3af9760d36141b847db366ec84e0dd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "791433990ef3a200525d34d7afd4510b",
+    "revision": "fb0e7c5e0356abd434d3d1c1c9643776",
     "url": "/marie-annemawhin.github.io/index.html"
   },
   {
-    "revision": "68ab8a529297ad756beb",
+    "revision": "f09f9f8f7a2d6b9fa8c7",
     "url": "/marie-annemawhin.github.io/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "68ab8a529297ad756beb",
-    "url": "/marie-annemawhin.github.io/static/js/2.4c4254a8.chunk.js"
+    "revision": "f09f9f8f7a2d6b9fa8c7",
+    "url": "/marie-annemawhin.github.io/static/js/2.23bb939b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/marie-annemawhin.github.io/static/js/2.4c4254a8.chunk.js.LICENSE.txt"
+    "url": "/marie-annemawhin.github.io/static/js/2.23bb939b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8edfdb88fe69cc90c4c6",
-    "url": "/marie-annemawhin.github.io/static/js/main.7c7a39d3.chunk.js"
+    "revision": "b2950cadf44d43f4de41",
+    "url": "/marie-annemawhin.github.io/static/js/main.9e2433c2.chunk.js"
   },
   {
     "revision": "f6a9eff01c56d5d2c3c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marie-annemawhin.github.io/precache-manifest.dc450f9335d71a8645e79be273d6f519.js"
+  "/marie-annemawhin.github.io/precache-manifest.4a3af9760d36141b847db366ec84e0dd.js"
 );
 
 self.addEventListener('message', (event) => {
