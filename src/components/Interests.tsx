f910e27eb4b20f48f4d25a149acee42503f14d3c
Paul Clark <clark.paul123@gmail.com>
2020-10-02 20:47:03 +0100

    feat: 🎸 responsiveness for interests

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -16,14 +16,17 @@ const items = [
 
 const Interests: React.FC = () => {
   const interestItems = items.map((item, index) => (
-    <p key={index} className="tc ph3 f4 f3-m f2-l fw2 black-90 mv3">
+    <p
+      key={index}
+      className="tc ph3 f4 f3-m f2-l fw2 black-90 mv3 fl w-100 w-33-m w-25-l"
+    >
       {item}
     </p>
   ))
   return (
     <Fragment>
       <SectionHeader title="Interests" />
-      {interestItems}
+      <div className="cf">{interestItems}</div>
     </Fragment>
   )
 }
