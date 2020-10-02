4d2be5545d7b2a53520313bec5f89108da227e08
Paul Clark <clark.paul123@gmail.com>
2020-10-02 20:34:37 +0100

    feat: 🎸 add some icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const sectionTitles = [
   'Bio',
@@ -43,6 +44,14 @@ const NavBar: React.FC = () => {
     <Fragment>
       <h1 className="f4 bold mw6">Marie-anne Mawhin</h1>
       <ul className="list pl0 ml0 mw6">{links}</ul>
+      <p className="f1">
+        <span className="pr3">
+          <FontAwesomeIcon icon={['fab', 'python']} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={['fab', 'github']} />
+        </span>
+      </p>
     </Fragment>
   )
 }
