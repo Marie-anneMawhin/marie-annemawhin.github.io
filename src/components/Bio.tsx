cdb071943c6616393f5a95840051e5542c8931e7
Paul Clark <clark.paul123@gmail.com>
2020-10-02 19:00:54 +0100

    feat: 🎸 improve bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -7,24 +7,39 @@ const Bio: React.FC = () => {
       <SectionHeader title="Bio" />
       <div className="f5 f4-m f3-l fw2 ">
         <p className="ph3 mv3">
-          Hey, I&apos;m Marie-anne. I&apos;m a biologist specialised in
-          cardiovascular immunology, and data avid scientist.
+          Hello, I&apos;m Marie-anne. I&apos;m a scientist with a background in
+          cardiovascular immunology, and a passion for data science and machine
+          learning.
         </p>
         <p className="ph3 mv3">
-          During my previous and current experience, I wrote several programs
-          for image analysis in python.
+          I&apos;m currently working as a research associate in cardiorenal
+          science, where I&apos;ve written several programs for image analysis
+          to automate and improve my work.
         </p>
         <p className="ph3 mv3">
-          I also wrote codes to analyse a large medical dataset from a clinical
-          trial and to analyse and visualise PCR results.
+          I&apos;ve also written Python scripts to clean and analyse a large
+          medical dataset from a clinical trial. I&apos;ve also written scripts
+          to analyse and visualise PCR results and plot evolutionary data.
         </p>
         <p className="ph3 mv3">
-          I found in coding the pleasure of scientific reasoning, the challenge
-          of problem solving and the joy of automation and efficiency.
+          I find great pleasure in the scientific reasoning, the challenge of
+          problem solving and the joy of automation and efficiency that
+          progamming offers.
         </p>
         <p className="ph3 mv3">
-          I have thus self-taught myself python and I am now trying to implement
-          Deep Learning for my images.
+          Thus far I&apos;m self-taught, and I am now trying to implement Deep
+          Learning for my images.
+        </p>
+        <p className="ph3 mv3">
+          I&apos;m keen to take my data science skills to the next level, and am
+          excited to have been selected for the Data Science For All (DS4A) -
+          Womens&apos;s Summit, and the Science 2 Data Science (S2DS)
+          programmes.
+        </p>
+        <p className="ph3 mv3">
+          I&apos;m interested in combining my biology knowledge with my data
+          science skills, especially in image anyalysis, machine learning and
+          computer vision.
         </p>
       </div>
     </Fragment>
