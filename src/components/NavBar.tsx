3576d2819a51d43ee28b147bdaf9145584edf19c
Paul Clark <clark.paul123@gmail.com>
2020-10-03 17:22:18 +0100

    fix: 🐛 mobile sizing in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,13 +27,13 @@ const NavBar: React.FC = () => {
   const links = sectionTitles.map((title, id) => (
     <li
       key={id}
-      className={`pv3 ${
+      className={`pv2 pv3-ns ${
         id !== sectionTitles.length - 1 ? 'bb ' : ''
       }b--washed-blue`}
     >
       <a
         onClick={() => scrollToId(title.toLowerCase())}
-        className="f6 f5-m f4-l fw5 link dim washed-blue bg-navy"
+        className="f6 f5-m f4-l fw4 link dim washed-blue bg-navy"
         href="#0"
       >
         {title}
@@ -42,9 +42,9 @@ const NavBar: React.FC = () => {
   ))
   return (
     <Fragment>
-      <h1 className="f4 bold mw6">Marie-anne Mawhin</h1>
+      <h1 className="f5 f4-ns f3-m f2-l bold fw6">Marie-anne Mawhin</h1>
       <ul className="list pl0 ml0 mw6">{links}</ul>
-      <p className="f1">
+      <p className="f3 f1-ns mv1 mv3-ns">
         <span className="pr3">
           <FontAwesomeIcon icon={['fab', 'python']} />
         </span>
