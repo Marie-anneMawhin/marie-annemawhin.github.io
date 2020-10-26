089a8b257110b9c90aaa94589a1995284a3c0495
Paul Clark <clark.paul123@gmail.com>
2020-10-26 18:30:00 +0000

    fix: 🐛 overflow on navbar on weird size screens

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Main from './components/Main'
 function App(): JSX.Element {
   return (
     <div className="App vh-100 avenir">
-      <div className="fl w-third w-30-m w-20-l h-100 pa2 pa3-ns bg-navy washed-blue o-80">
+      <div className="fl w-third w-30-m w-20-l h-100 overflow-y-scroll pa2 pa3-ns bg-navy washed-blue o-80">
         <NavBar />
       </div>
       <div className="fl w-two-thirds w-70-m w-80-l pa2 pa3-ns h-100 overflow-y-scroll bg-washed-blue">
