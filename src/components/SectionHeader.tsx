d2d7742a713b12668625e7784384976e9b63bf75
Paul Clark <clark.paul123@gmail.com>
2021-03-13 12:22:41 +0000

    feat: 🎸 decrease font weight and size

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 const SectionHeader: React.FC<{ title: string }> = (props: {
   title: string
 }) => (
-  <p id={props.title.toLowerCase()} className="ph3 pv3 f3 f2-m fw6 f1-l navy">
+  <p id={props.title.toLowerCase()} className="ph3 pv3 f4 f3-m f2-l fw5 navy">
     {props.title}
   </p>
 )
