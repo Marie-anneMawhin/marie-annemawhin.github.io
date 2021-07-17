e0b3b6c5d17debe5583552dd6b776a6c4346ef16
Paul Clark <clark.paul123@gmail.com>
2021-07-17 15:01:04 +0100

    fix: 🐛 Change padding to margin

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -4,7 +4,7 @@ const SectionHeader: React.FC<{ title: string }> = (props: {
 }) => (
   <div
     id={props.title.toLowerCase()}
-    className="ph3 pt4 pb3 f4 f3-m f2-l fw5 navy"
+    className="mh3 mt4 mb3 f4 f3-m f2-l fw5 navy"
   >
     {props.title}
   </div>
